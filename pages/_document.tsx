522a53d46ce73150ca26e5687ba4477243c4f561
marlon passos <marlon.passos@m3ecommerce.com>
2024-01-23 01:48:30 -0300

    test 1

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,13 +4,13 @@ function Document() {
   return (
     <Html>
       <Head>
-        <link rel="icon" href="https://www.elastobor.com.br/favicon.ico" />
+        {/* <link rel="icon" href="https://www.elastobor.com.br/favicon.ico" /> */}
         <meta name="robots" content="index,follow" />
         <meta property="og:image:alt" content="Imagem da Elastobor" />
         <meta property="og:type" content="website" />
 
         {/* eslint-disable-next-line @next/next/no-sync-scripts */}
-        <script src="https://cdn.jsdelivr.net/gh/MarlonPassos-git/stupid-third-party/stupid-modal.js"></script>
+        {/* <script src="https://cdn.jsdelivr.net/gh/MarlonPassos-git/stupid-third-party/stupid-modal.js"></script> */}
         {/* <script dangerouslySetInnerHTML={{
           __html: `
            window.onload = function ()  {
